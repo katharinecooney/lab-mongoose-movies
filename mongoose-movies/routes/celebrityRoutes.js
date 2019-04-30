50653b1296d70db78c29994e2fb654fd0bcca5e1
Katharine Cooney <katharinecooney@gmail.com>
2019-04-30 08:05:40 +0200

    Add functional celebrity details route

--- mongoose-movies/routes/celebrityRoutes.js
+++ mongoose-movies/routes/celebrityRoutes.js
@@ -8,14 +8,37 @@ router.get('/', (req, res, next) => {
     .catch((error) => {console.log(error)});
 });
 
+
 // 'ID details'
 router.get('/:id', (req, res, next) => {
-  const { celebId } = req.params;
-
-  Celebrity.findById(celebId)
-
-    .then( (celeb) => res.render('celebrities/show', {celeb}))
+  console.log(req.params.id);
+  Celebrity.findById(req.params.id)
+    .then( celeb => res.render('celebrities/show', {celeb}))
     .catch( (err) => console.log(err));
-})
+});
+
+// QUESTION! WHY DOES THE CODE NOT WORK WHEN WE DECONSTRUCT REQ.PARAMS.ID? 
+// router.get('/:id', (req, res, next) => {
+//   console.log(req.params.id)
+//   const { celebId } = req.params.id;
+//   Celebrity.findById(celebId)
+//     .then( celeb => res.render('celebrities/show', {celeb}))
+//     .catch( (err) => console.log(err));
+// })
+
+
+
+
+router.get('/new', (req, res, next) => {
+  res.render('/celebrities/new');
+});
+
+router.post('/celebrities', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const newCelebrity = new Celebrity({name, occupation, catchPhrase});
+  newCelebrity.save()
+    .then(celebrity => res.redirect('/celebrities'))
+    .catch(error => res.redirect('/new'));
+});
 
 module.exports = router;
